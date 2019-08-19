43db46e835cc7e6706823bbf5a99e8e0de583eb9
AshlyantonyBabu <elsyantonybabu@gmail.com>
2019-08-19 16:35:06 +0530

    mongo edit delete

--- router/emprout.js
+++ router/emprout.js
@@ -41,3 +41,61 @@ router.post("/add",function(req,res){
     })
    // res.send('employee added')
 })
+router.get("/edit/:id",function(req,res){
+    var idi=req.params.id
+    console.log(idi)
+    var qry={eid:idi}
+    emp.findOne(qry,function(err,result){
+        if(err)throw err
+        else{
+            res.render('editemp',{empes:result})
+            
+            console.log(result)
+        }
+    })
+    
+   // res.send(idi)
+})
+router.post("/update/:id",function(req,res){
+    var idi=req.params.id
+    //var e2=new emp();
+    eidi=req.body.eid
+    namee=req.body.ename
+    salaryy=req.body.esalary
+
+    var myquery = { eid: idi };
+  var newvalues = { $set: {eid:eidi,name: namee, salary: salaryy } };
+  emp.updateOne(myquery, newvalues, function(err,result) {
+    if (err) throw err;
+   
+   // db.close();
+   else{
+    console.log("1 document updated");
+   // res.rendir('viewemp')
+res.redirect('/emp/view')
+}
+  
+  });
+//   res.render('viewemp')
+   
+})
+router.get("/delete/:id",function(req,res){
+    var idi=req.params.id
+    var qry={eid:idi}
+    emp.deleteOne(qry,function(err,obj){
+        if(err)throw err
+        else{
+            res.redirect('/emp/view')
+        }
+    })
+})
+router.post("/search",function(req,res){
+    var seachvalue=req.body.search;
+    //var qry={name:{$text:{$search:seachvalue}}}
+    emp.find({$text: { $search: seachvalue } },function(err,result){
+        if(err)throw err
+        else{
+            res.render("viewemp",{emps:result})
+        }
+    })
+})
